23bde1930dbe3d6196faf35fba5fdc1a6179d895
harshad <Harshadkajale333@gmail.com>
2023-01-31 00:16:49 +0530

    first commit

---
+++ src/Components/rightContainer/RightContainer2/RightContainer2.js
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+import { Avatar } from "@mui/material";
+import style from "./RightContainer2.module.css";
+import CustomButton from "../../../Atom/Button/CustomButton";
+import VerifiedIcon from "@mui/icons-material/Verified";
+
+function Righthomebox2() {
+  const handleClick = (id) => {
+    const updatedFollowContainer = followContainer.map((item) => {
+      if (item.id === id) {
+        item.followed = !item.followed;
+      }
+      return item;
+    });
+    setFollowContainer(updatedFollowContainer);
+  };
+  const [followContainer, setFollowContainer] = useState([
+    {
+      id: 1,
+      tag: <i className="far fa-solid fa-badge-check"></i>,
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjj_oz7crwHtUQj0jJuvLq-ILk5oikP_3FFL-mDJqk&s",
+      text: "virat",
+      text2: "virat@gmail.com",
+    },
+    {
+      id: 2,
+      tag: <i className="fa fa-solid fa-badge-check"></i>,
+      src: "https://www.cricketsoccer.com/wp-content/uploads/2020/09/i.jpg",
+      text: "Rohit",
+      text2: "rohits@gmail.com",
+    },
+    {
+      id: 3,
+      tag: <i className="fa fa-solid fa-badge-check"></i>,
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjj_oz7crwHtUQj0jJuvLq-ILk5oikP_3FFL-mDJqk&s",
+      text: "mahi",
+      text2: "virat@gmail.com",
+    },
+    {
+      id: 4,
+      tag: <i className="fa fa-solid fa-badge-check"></i>,
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjj_oz7crwHtUQj0jJuvLq-ILk5oikP_3FFL-mDJqk&s",
+      text: "virat",
+      text2: "virat@gmail.com",
+    },
+  ]);
+  return (
+    <div className={style.container}>
+      <h1>Who to follow</h1>
+      <div className={style.main}>
+        {followContainer.map((menu) => {
+          return (
+            <div className={style.wrapper}>
+              <Avatar alt="Remy Sharp" src={menu.src} />
+              <div className={style.img}>
+                <h3>
+                  {menu.text}
+                  <span style={{ color: "blue" }}> {menu.tag} </span>
+                  <VerifiedIcon style={{ color: "blue", fontSize: "20px" }} />
+                </h3>
+                <h6>{menu.text2}</h6>
+              </div>
+              <div className={style.btntxt}>
+                <CustomButton
+                  buttonText={menu.followed ? "Following" : "Follow"}
+                  btnNext={() => handleClick(menu.id)}
+                  key={menu.id}
+                  customCss={style.follwButton}
+                  disabled={menu.followed}
+                />
+              </div>
+            </div>
+          );
+        })}
+        <div>
+          <CustomButton
+            customCss={style.btn2}
+            buttonText={"see more"}
+            btnNext={() => alert("see more things")}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Righthomebox2;
